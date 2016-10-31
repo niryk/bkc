6da5b0b709021dd05e08d7bb0271939e67652f1f
niryk <nir@yakobovski.com>
2016-11-01 01:01:53 +0200

    autocomplete & fuck

---
+++ bashrc/autocomplete.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo set completion-ignore-case on | sudo tee -a /etc/inputrc
---
+++ pkgs/fuck.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo apt update
+sudo apt install python3-dev python3-pip
+sudo -H pip3 install thefuck
